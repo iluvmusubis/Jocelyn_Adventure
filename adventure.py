174554e28df9b57dbfc15c58bab082ec554fbfc2
Alex <alexmelford555gmail.com>
2017-10-27 10:28:20 -0700

    Initial commit

---
+++ adventure.py
@@ -0,0 +1,220 @@
+def Audrey():
+      print("\nYou decide to follow Audrey. However, Audrey had very little sleep last night. Because of her lack of\n"
+            "\nsleep and low motivation to run around for money, Audrey decides to step aside and take a nap at the\n"
+            "\nclosest bench. Since Audrey is taking her nap, you end getting even more lost and confused, thus creating\n"
+            "\na big gap between you and the other contestants. In the end, you wasted1 too much time on the first clue\n"
+            "\nand end up losing the game.\n")
+      exit()
+
+def own_way():
+      print("\nYou decide to leave Audrey behind and find the balloon stand yourself. However, there are three main\n"
+            "\npathways. You can go left, straight, or take a right. You decide to:\n"
+            "\n1. Go left\n"
+            "\n2. Go straight\n"
+            "\n3. Go right\n")
+
+
+def left():
+      print("\nSo you make a left and head in that direction only to find that you have entered the petting zoo section\n"
+            "\nof the carnival. However, you forget your way back and end up never even finding the balloon stand for\n"
+            "\nthe second clue. Since you don't have the second clue, you end up losing the game\n")
+      exit()
+
+#letter H
+def straight():
+      print("\nSo you head straight and the sound of game noises and kids cheering grows louder. You start to realize that\n"
+            "\nyou chose the right direction and are heading towards the game section of the carnival. As you get closer,\n"
+            "\nyou spot a man in a striped shirt holding a balloon next to the fish pong station. Making a beeline towards\n"
+            "\nhim, you finally reach him and he hands you your next clue.You quickly rip open the envelope and find a\n"
+            "\nletter H and a clue saying go to the place where where you can pet a furry animal that makes the sound:\n"
+            "\nBAAAAA. You realize that the next clue is at the petting zoo. As you try to figure out your route, you notice\n"
+            "\nthat both Bobby and Dave just found the second clue. Instead of finding the next clue on your own, you\n"
+            "\ndecide to follow:\n"
+            "\n1. Bobby\n"
+            "\n2. Dave\n")
+
+def right():
+      print("\nSo you make a right and head in that direction only to find that you have entered the 'kid zone' section\n"
+            "\nof the carnival. However, you forget your way back and end up never even finding the balloon stand for the\n"
+            "\nsecond clue. Since you don't have the second clue, you end up losing the game\n")
+      exit()
+
+def bobby():
+      print("\nYou end up choosing to follow Bobby since he appears calm and knows what he's doing. He heads to the left and\n"
+            "\nyou follow right behind. After a couple of minutes, the smell of hay and horses begins to fill your nose.\n"
+            "\nLuckily for you, you decided to follow the right person as Bobby brought you straight to the petting zoo!\n"
+            "\nAt the petting zoo, you quickly begin searching for a petting zoo with sheep. Fortunately for you, Bobby\n"
+            "\nhas incredible hearing and finds the sheep right away and you follow along. At the sheep area of the petting\n"
+            "\nzoo, a man in charge of the station hands you another envelope. You rip open the envelope to find a letter\n"
+            "\nT and a clue saying that you will find your next clue at a place where you go in a circle. At that place,\n"
+            "\nthe woman in charge of the ride will hand you your next clue. You figure it out and head towards:\n"
+            "\n1. the carousel ride\n"
+            "\n2. the spinning teacups\n"
+            "\n3. the ferris wheel\n")
+
+def dave():
+      print("\nYou end up choosing to follow Dave since you know he's a try hard. Dave ends up heading to the right and\n"
+            "\nyou follow right behind. After a couple of minutes, it seems like you're going the wrong way, but you still\n"
+            "\nhave faith that Dave knows where he's going so you continue to follow him. Unfortunately, after a good 15\n"
+            "\nminutes, you end up at the 'kid zone.' You realize you made the wrong choice in following Dave and end up\n"
+            "\nlosing the game.\n")
+      exit()
+
+def teacups():
+      print("\nYou solve the clue and start heading towards the spinning teacups. In a couple of minutes, you realize that\n"
+            "\nyou can't reach the lady in charge unless you wait in line. However, the line is about 30 minutes long, but\n"
+            "\nyou still decide to wait in line. Thirty minutes later, you are finally at the front of the line. You walk\n"
+            "\nup to the lady in charge and end up discovering that she doesn't have your clue. Because you spent 30 minutes\n"
+            "\nwaiting, you wasted too much time, placing you in last place. Unfortunately, you couldn't catch up and you\n"
+            "\nlose the game.\n")
+      exit()
+
+#letter o
+def carousel():
+      print("\nYou solve the clue and start heading towards the carousel. Luckily for you, the ride just started so you\n"
+            "\nare the first person in line. You walk up to the front of the line and approach the lady in charge. She\n"
+            "\nthen hands you an envelope. Inside, you find a letter o and a clue stating to go to the place where you\n"
+            "\ncan take a picture with someone who has a big red nose and blows up balloons in the shape of food. You\n"
+            "\nfigure out that this person must a clown. However, as you begn your search for this clown, you start to\n"
+            "\nfeel lightheaded. You decide to:\n"
+            "\n1. Continue looking for the clown\n"
+            "\n2. Take a quick break\n"
+            "\n3. Grab something to eat\n")
+
+def ferris():
+      print("\nYou solve the clue and start heading towards the ferris wheel. In a couple of minutes, you realize that you\n"
+            "\ncan't reach the lady in charge unless you wait in line. However, the line is about an hour long, but you\n"
+            "\nstill decide to wait in line. An hour later, you are finally at the front of the line. You walk up to the\n"
+            "\nlady in charge and end up discovering that she doesn't have your clue. Because you spent an hour waiting,\n"
+            "\nyou wasted too much time placing you in last place. Unfortunately, you couldn't catch up and you lose the\n"
+            "\ngame.\n")
+      exit()
+
+def tired():
+      print("\nYou feel that you are already behind, so you continue searching. Despite your moitvation and perseverence\n"
+            "\nto keep going, your body is very dehydrated and ends up failing you. You become too weak to move on and\n"
+            "\nyou end up never finishing the game.\n")
+      exit()
+
+def clown():
+      print("\nSo you decide to take a break and grab a sandwich. While waiting for your sandwich, something catches your\n"
+            "\neye. You do a double take and realize the clown making balloons in the shape of food is standing right\n"
+            "\nacross from you. You quickly run across and get your next clue. You read the paper and it tells you that\n"
+            "\nthis is your last clue in finding the finish line. It instructs you to put all of your letters together to\n"
+            "\nthen form a word. Then it tells you that the second word of the answer is the opposite of a cat. The finish\n"
+            "\nline will be at this stand. Go back and look at all of your letters. Once you figure it out, you head\n"
+            "\ntowards:\n"
+            "\n1. the hotcake stand\n"
+            "\n2. the corn dog stand\n"
+            "\n3. the hot dog stand\n")
+
+def sleep():
+      print("\nYou can't take the pain anymore and you feel too weak to move on. You spot an empty bench to your left\n"
+            "\n and take a seat. However, becuase you feel so tired, you lie down on the bench and enp up falling asleep.\n"
+            "\nYou take a nap for what seems like months until someone gently nudges you. You slowly open your eyes only\n"
+            "\nto find an emplyee staring straight down at you saying, 'Excuse me. The Carnival is closing in 5 minutes.\n"
+            "\nYou will need to leave soon.' You wake up and take a look at your watch only to find out its already past\n"
+            "\n10 PM. You realize that you slept through the rest of the game and you never even finished it.\n")
+      exit()
+
+def hotcake():
+      print("\nUnfortunately, you made the wrong decision and you lost the game :(\n")
+
+def corn_dog():
+      print("\nUnfortunately, you made the wrong decision and you lost the game :(\n")
+
+def winner():
+      print("\nYou walk up to the hot dog stand and see the stout man in the checkered shirt standing there. You quickly\n"
+            "\nrun to him only to find out that you're the first one to have arrived meaning that you won! You just won\n"
+            "\n$1,000,000! Congrats and thanks for playing.\n")
+
+def main():
+      print("\n\n")
+      print("The Carnival Scavenger Hunt: A Choose your own Adventure!")
+      print("\n\n"
+            "\nA couple months ago, you entered a game show to participate in a scavenger hunt to win $1,000,000. Just\n"
+            "\nrecently, you received a letter in the mail notifying you that you have been chosen as a contestant.\n"
+            "\nHowever, you will also be competing against 10 other contestants for the same prize. Each contestant will\n"
+            "\nbe given a starting clue to then find their next clue. After finding each clue, you will be given an\n"
+            "\nadditional clue and a paper with a letter on it. There will be 3 letters and 1 extra clue in total that\n"
+            "\nspell out a carnival booth, which will be the final stop. The first person to arrive with all of the letters\n"
+            "\nwill win the prize. The scavenger hunt is a week away, but time flies by quickly. The days pass by and game\n"
+            "\nday has finally. You drive up to the carnival parking lot and reread the letter which instructs you to meet\n"
+            "\nat the front entrance.\n")
+main()
+
+print("\n\n")
+
+#letter O & intro
+print("\nYou walk up to the front entrance and already see a group of people who you suspect are your opponents. At the\n"
+      "\nentrance you introduce yourself and meet a couple of the other contestants: Bobby, Sarah, Dave, and Audrey. While\n"
+      "\nwaiting for other contestants, you discover that Audrey doesn't seem very motivated, Dave is very intense and\n"
+      "\ndetermined to win, Bobby seems a little reserved, and Sarah is a genius. Once everyone arrives, a stout man dressed\n"
+      "\nin slacks and a black and white checkered button down raises his voice and welcomes everyone. He then begins to\n"
+      "\nread over the instructions. After a few minutes, everyone receives an envelope, but you are instructed to not\n"
+      "\nopen it until you hear the bell. Riinnnggg!! The bell rings and everyone tears open their enevelopes. In it you\n"
+      "\nfind clue instructing you to find the man in the striped shirt at the balloon stand which is located next to the\n"
+      "\nfish pong station. You look up and see that almost everyone has left except Audrey. You decide\n"
+      "\nto:\n"
+      "\n1. Follow Audrey\n"
+      "\n2. Go your own way\n")
+
+choice = input("enter your choice")
+
+print("\n\n")
+
+if (choice == 1):
+      Audrey()
+else:
+      own_way()
+
+
+
+direction = input("enter the direction that you go")
+
+print("\n\n")
+if (direction == 1):
+      left()
+elif (direction == 2):
+      straight()
+else:
+      right()
+
+
+follow = input("enter the person that you will follow")
+
+print("\n\n")
+if (follow == 1):
+      bobby()
+else:
+      dave()
+
+ride = input("enter your answer to the clue")
+print("\n\n")
+if (ride == 1):
+      carousel()
+elif(ride == 2):
+      teacups()
+else:
+      ferris()
+
+rest = input("enter the action you will do next")
+
+print("\n\n")
+if (rest == 1):
+      tired()
+elif (rest == 3):
+      clown()
+else:
+      sleep()
+
+finish = input("enter the place where the finish line is")
+
+print("\n\n")
+if (finish == 1):
+      hotcake()
+elif (finish == 2):
+      corn_dog()
+else:
+      winner()
+
